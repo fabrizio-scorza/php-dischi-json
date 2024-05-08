1f27a7295c58eb00ef29e3231b8ad298c65973d6
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-08 16:13:44 +0200

    ADD -  array from js

--- index.php
+++ index.php
@@ -12,7 +12,34 @@
 <body>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
-    <div id="app">
+    <div id="app" class="vh-100">
+
+        <!-- header -->
+        <header class="p-3 bg-success bg-gradient text-success-emphasis">
+            <div class="container">
+                <nav>
+                    LOGO
+                </nav>
+            </div>
+        </header>
+
+        <!-- main -->
+        <main class=" py-3 bg-dark text-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <div v-for="disc in discs" class="card text-center">
+                            <img :src="disc.poster" alt="" class=" card-img-top">
+                            <div class="card-body">
+                                <h2 class="card-title"> {{ disc.title }}</h2>
+                                <h5 class="card-subtitle"> {{ disc.author }}</h5>
+                                <h3 class="card-text"> {{ disc.year}}</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
 
     </div>
 
